6de0265e3bfdfef0efc46427f672922d14fae1f1
CristianoZitarosa <36627397+CristianoZitarosa@users.noreply.github.com>
2019-05-05 19:55:37 +0200

    Create the choice bar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../style/App.css';
+import Choice from './Choice.js';
 
 function App() {
   return (
     <div className="App">
-
+      <Choice/>
     </div>
   );
 }
---
+++ src/components/Choice.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../style/Choice.css';
+
+function Choice() {
+  return (
+    <div className="choice">
+      <p className="selectionText">
+        Select an App:
+      </p>
+      <div className="selectionContainer">
+        <div className="appSelection">
+          <img width="75" height="75" src="https://s3-us-west-2.amazonaws.com/dexcommisc/App+images/g6-app.png" alt="G6 App"/>
+          <p>G6 Mobile App</p>
+        </div>
+        <div className="appSelection">
+          <img width="75" height="75" src="https://s3-us-west-2.amazonaws.com/dexcommisc/App+images/Mobile_Icon.png" alt="G5 App"/>
+          <p>G5 Mobile App</p>
+        </div>
+        <div className="appSelection">
+          <img width="75" height="75" src="https://s3-us-west-2.amazonaws.com/dexcommisc/App+images/clarity.jpg" alt="Clarity App"/>
+          <p>Clarity App</p>
+        </div>
+        <div className="appSelection">
+          <img width="75" height="75" src="https://s3-us-west-2.amazonaws.com/dexcommisc/App+images/follow-app.png" alt="Follow App"/>
+          <p>Follow App</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Choice;
