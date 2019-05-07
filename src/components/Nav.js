76071420e65a56014efa31e2a35f70ce9cb0ddad
CristianoZitarosa <36627397+CristianoZitarosa@users.noreply.github.com>
2019-05-07 19:12:58 +0200

    Set how to close overlays

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../style/Nav.css';
 
 function Nav() {
   return (
     <div className="btn-group">
-      <button className="button btn-left"><i className="fas fa-home"></i> Home</button>
-      <button className="button btn-right"><i className="fas fa-exclamation-circle"></i> About this page</button>
+      <Link to="/">
+        <button className="button btn-left"><i className="fas fa-home"></i> Home</button>
+      </Link>
+      <Link to="/test">
+        <button className="button btn-right"><i className="fas fa-exclamation-circle"></i> About this page</button>
+      </Link>
     </div>
   );
 }
--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../style/Overlay.css';
 
 function Overlay() {
+
+  function closeOverlay() {
+    document.querySelector('.overlay').style.display = 'none';
+  }
+
   return (
     <div className="overlay">
       <div id="landingMessage">
         <p>Please read carefully:</p>
         <p>This is NOT an official page mantained from Dexcom!</p>
-        <button type="button" name="getInformed" id="getInformed"><i className="fas fa-info-circle pulse"></i> Get Informed </button>
-        <button type="button" name="agreeButton" id="agreeButton"><i className="fas fa-thumbs-up pulse"></i> I Agree </button>
+        <Link to="/test">
+          <button type="button" name="getInformed" id="getInformed">
+            <i className="fas fa-info-circle pulse"></i> Get Informed
+          </button>
+        </Link>
+        <button type="button" name="agreeButton" id="agreeButton" onClick={closeOverlay}>
+          <i className="fas fa-thumbs-up pulse"></i> I Agree
+        </button>
       </div>
       <div className="emptyOverlay"></div>
     </div>
