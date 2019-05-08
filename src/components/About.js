066dfc0e50a64c28911bc038186b0de8cc1b91f4
CristianoZitarosa <36627397+CristianoZitarosa@users.noreply.github.com>
2019-05-08 18:45:33 +0200

    Patch link in About page

--- src/components/About.js
+++ src/components/About.js
@@ -18,7 +18,7 @@ class About extends Component {
     return (
       <div id="aboutMessage">
         <p><strong>This is an Unofficial page!</strong></p>
-        <p>Dexcom is the owner of the brand, images of the Apps, style of the page, and the administrator of <strong>the original page</strong> you can reach <a href="https://www.dexcom.com/dexcom-international-compatibility" target="_blank" rel="noopener noreferrer"><strong>by this link</strong></a>.</p>
+        <p>Dexcom is the owner of the brand, images of the Apps, style of the page, and the administrator of <strong>the original page</strong> you can reach <a href="https://www.dexcom.com/dexcom-international-compatibility" target="_blank" rel="noopener noreferrer"><strong id="linkDex">by this link</strong></a>.</p>
         <p>The style of this page is as much possible similar to the original page only to avoid that users can feel lost due a different graphic element.</p>
         <p>This page is made from a Dexcom User to help Dexcom users.</p>
         <p>The purpose is to provide an easier consultation of supported devices by original Dexcom Apps since the original page could currently be difficult to read to some users.</p>
