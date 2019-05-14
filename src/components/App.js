6c3be7a4c616da046f6b6d0a1b8b7fb2dd459e4e
CristianoZitarosa <36627397+CristianoZitarosa@users.noreply.github.com>
2019-05-14 20:31:52 +0200

    Set localStorage to avoid overlay if user already visited the page

--- src/components/App.js
+++ src/components/App.js
@@ -9,15 +9,35 @@ import AppComponent from './AppComponent.js';
 
 class App extends Component {
 
+  componentDidMount() {
+    this.checkStorage();
+  }
+
   closeOverlay = () => {
     document.querySelector('.overlay').classList.add('close');
     document.body.classList.add("scroll");
   };
 
+  setStorage = () => {
+    localStorage.setItem("isNewUser", false);
+    console.log(`There was no localStorage before, I have set "${localStorage.isNewUser}" right now.` );
+  };
+
+  checkStorage = () => {
+    if (localStorage.isNewUser !== undefined) {
+      console.log(`The localStorage "${localStorage.isNewUser}" is already set, no action is needed.`);
+      this.closeOverlay();
+    }
+
+  };
+
   render() {
     return (
       <div>
-        <Overlay closeOverlay={ this.closeOverlay }/>
+        <Overlay
+          closeOverlay={ this.closeOverlay }
+          setStorage={ this.setStorage }
+        />
         <Nav />
         <p id="barMessage">
           <span className="homeMessage">Select an App:</span>
--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -3,6 +3,12 @@ import { Link } from 'react-router-dom';
 import '../style/Overlay.css';
 
 class Overlay extends Component {
+
+  setValues = () => {
+    this.props.closeOverlay();
+    this.props.setStorage();
+  }
+
   render() {
 
     return (
@@ -11,11 +17,11 @@ class Overlay extends Component {
           <p>Please read carefully:</p>
           <p>This is NOT an official page maintained by Dexcom!</p>
           <Link to="/about">
-          <button type="button" name="getInformed" id="getInformed" onClick={ this.props.closeOverlay }>
+          <button type="button" name="getInformed" id="getInformed" onClick={ this.setValues }>
             <i className="fas fa-info-circle pulse"></i> Get Informed
           </button>
         </Link>
-        <button type="button" name="agreeButton" id="agreeButton" onClick={ this.props.closeOverlay }>
+        <button type="button" name="agreeButton" id="agreeButton" onClick={ this.setValues }>
           <i className="fas fa-thumbs-up pulse"></i> Agree
         </button>
       </div>
