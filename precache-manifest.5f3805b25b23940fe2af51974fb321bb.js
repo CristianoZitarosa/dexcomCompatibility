ddb67f23ac90c3aa08e486fc436f39739689e83a
CristianoZitarosa <36627397+CristianoZitarosa@users.noreply.github.com>
2019-07-05 15:47:26 +0200

    Updates

---
+++ precache-manifest.5f3805b25b23940fe2af51974fb321bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a99008097ebc005963a0fe63bc5600",
+    "url": "/dexcomCompatibility/index.html"
+  },
+  {
+    "revision": "5c9165d77251e5510272",
+    "url": "/dexcomCompatibility/static/css/main.8f9fa27d.chunk.css"
+  },
+  {
+    "revision": "412bde2d8ff41300ff21",
+    "url": "/dexcomCompatibility/static/js/2.ffb7add5.chunk.js"
+  },
+  {
+    "revision": "5c9165d77251e5510272",
+    "url": "/dexcomCompatibility/static/js/main.2d40d9a6.chunk.js"
+  },
+  {
+    "revision": "02771005846b329871f3",
+    "url": "/dexcomCompatibility/static/js/runtime~main.40868e22.js"
+  }
+]);
