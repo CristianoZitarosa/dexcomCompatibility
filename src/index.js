0561acf2fa714db1226dd5bbf573e2d54074faec
CristianoZitarosa <36627397+CristianoZitarosa@users.noreply.github.com>
2019-07-08 18:35:40 +0200

    Add basename

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrowserRouter><App /></BrowserRouter>,
+  <BrowserRouter basename="/dexcomCompatibility"><App /></BrowserRouter>,
   document.getElementById('root')
 );
 
