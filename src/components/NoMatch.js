4af993f93aa2a229304b87ad7e217ff79eb476ce
CristianoZitarosa <36627397+CristianoZitarosa@users.noreply.github.com>
2019-07-17 18:16:35 +0200

    Fix no landmark axe warning for noMatch page

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -7,13 +7,13 @@ class NoMatch extends Component {
 
     return (
 
-      <div id='nomatch'>
+      <main id='nomatch'>
         <img id="emoji" width="150" height="150" src="./images/loudly-crying-face.png" alt="emoji"/>
         <h1>Page not found!</h1>
         <Link to="/">
           <button id='nomatchBtn' onClick={ this.props.storage }><i className="fas fa-backward pulse"></i> <strong>Back to Home</strong></button>
         </Link>
-      </div>
+      </main>
 
     );
 
